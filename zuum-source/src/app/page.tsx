b31101a55639b455e57cbfdd2804119f36a8fddf
ZUUMEV <zuumev@gmail.com>
2026-08-16 22:19:41 +0530

    Add JSON-LD schemas for LocalBusiness and FAQ

--- zuum-source/src/app/page.tsx
+++ zuum-source/src/app/page.tsx
@@ -10,24 +10,31 @@ import { Impact } from '@/components/sections/impact'
 import { CTA } from '@/components/sections/cta'
 import { Footer } from '@/components/sections/footer'
 import { FloatingWhatsApp } from '@/components/sections/floating-whatsapp'
+import { LocalBusinessSchema, FAQSchema } from '@/components/json-ld'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <main className="flex-1">
-        <Hero />
-        <PartnerMarquee />
-        <HowToJoin />
-        <Plans />
-        <Benefits />
-        <Compare />
-        <FAQ />
-        <Impact />
-        <CTA />
-      </main>
-      <Footer />
-      <FloatingWhatsApp />
-    </div>
+    <>
+      {/* JSON-LD Structured Data for Google Rich Results */}
+      <LocalBusinessSchema />
+      <FAQSchema />
+
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <main className="flex-1">
+          <Hero />
+          <PartnerMarquee />
+          <HowToJoin />
+          <Plans />
+          <Benefits />
+          <Compare />
+          <FAQ />
+          <Impact />
+          <CTA />
+        </main>
+        <Footer />
+        <FloatingWhatsApp />
+      </div>
+    </>
   )
 }
